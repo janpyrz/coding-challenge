a38ab8f1c333c44bd6000110fef62e741761a502
Janek Pyrz <janpyrz@gmail.com>
2020-05-18 21:14:58 +0200

    Add search customer tests

---
+++ src/com/company/pages/SearchCustomersPage.java
@@ -0,0 +1,42 @@
+package com.company.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class SearchCustomersPage {
+    WebDriver driver;
+    By searchInput = By.id("search-input");
+    By searchSelect = By.id("search-column");
+    By table = By.tagName("tbody");
+    By matchCase = By.id("match-case");
+
+    public SearchCustomersPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void searchFor(String text) {
+        WebElement searchElement = driver.findElement(searchInput);
+        searchElement.clear();
+        searchElement.sendKeys(text);
+    }
+
+    public void selectAttributeByValue(String value) {
+        Select dropdown = new Select(driver.findElement(searchSelect));
+        dropdown.selectByValue(value);
+    }
+
+    public int getTableRowCount() {
+        WebElement tableElement = driver.findElement(table);
+        return tableElement.findElements(By.tagName("tr")).size();
+    }
+
+    public String getTableText() {
+        return driver.findElement(table).getText();
+    }
+
+    public void checkMatchCaseBox() {
+        driver.findElement(matchCase).click();
+    }
+}
---
+++ src/com/company/tests/TestSearchCustomer.java
@@ -0,0 +1,55 @@
+package com.company.tests;
+
+import com.company.pages.SearchCustomersPage;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestSearchCustomer {
+    String driverPath = "C:\\Program Files\\Java\\geckodriver.exe";
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", driverPath);
+
+        driver = new FirefoxDriver();
+        driver.get("file:///C:/Users/Janek/IdeaProjects/Automation/ui/index.html");
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void testSearchByEmail() {
+        SearchCustomersPage searchCustomersPage = new SearchCustomersPage(driver);
+
+        String email = "conatact@postimex.pl";
+
+        searchCustomersPage.searchFor(email);
+        searchCustomersPage.selectAttributeByValue("Email");
+
+        Assert.assertTrue(searchCustomersPage.getTableText().contains(email));
+        Assert.assertEquals(1, searchCustomersPage.getTableRowCount());
+    }
+
+    @Test
+    public void testMatchCase() {
+        SearchCustomersPage searchCustomersPage = new SearchCustomersPage(driver);
+
+        searchCustomersPage.searchFor("alabaster");
+        Assert.assertEquals(1, searchCustomersPage.getTableRowCount());
+
+        searchCustomersPage.checkMatchCaseBox();
+        Assert.assertEquals(0, searchCustomersPage.getTableRowCount());
+
+        searchCustomersPage.searchFor("Alabaster");
+        Assert.assertEquals(1, searchCustomersPage.getTableRowCount());
+    }
+}
